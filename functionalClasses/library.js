be2d22daca30aec804f39e4a07d5bc0f2c0afdf9
David Vite <vitebasket10@gmail.com>
2016-05-02 17:35:21 -0600

    Functional shared pattern before fix

--- functionalClasses/library.js
+++ functionalClasses/library.js
@@ -1,13 +1,23 @@
-// In order to avoid duplication we need to put the some fucntions out of th econstructor
-// Also we change obj for the parameter this in the move function becuase It won't have access
-// this replaces obj because this function is being called as a method on objects created with car
+// imagine that now we want to add more functions and assigning each one as a property of the object, we would still having the issue 
+// of naming it in two places the first is when we are building the function and the second is when we want to assign it over
+// onto every car object . 
 
 var Car = function(loc){
 	var obj= {loc: loc};
 	obj.move = move;
+	obj.on = on;
+	obj.off = off;
 	return obj;
 	};
 
 var move = function(){
 	this.obj++;
 };
+
+var on = function(){
+	// something here
+};
+var off = function(){
+	// something else here
+}
+
