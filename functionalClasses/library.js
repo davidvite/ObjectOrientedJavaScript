6784c2b9d9b7630678a43153f9166f417d7a336f
David Vite <vitebasket10@gmail.com>
2016-05-03 10:03:24 -0600

    Functional shared pattern after fix it.

--- functionalClasses/library.js
+++ functionalClasses/library.js
@@ -1,23 +1,21 @@
-// imagine that now we want to add more functions and assigning each one as a property of the object, we would still having the issue 
-// of naming it in two places the first is when we are building the function and the second is when we want to assign it over
-// onto every car object . 
+// there is a way to include all the methods we want on every car object as a resul of having them in a list somewhere.
+// extend methods using extend and creating an object with the methods.
 
 var Car = function(loc){
 	var obj= {loc: loc};
-	obj.move = move;
-	obj.on = on;
-	obj.off = off;
+	extend(obj, methods);
 	return obj;
 	};
 
-var move = function(){
+var methods = {
+	move : function(){
 	this.obj++;
-};
-
-var on = function(){
+	},
+ 	on = function(){
 	// something here
-};
-var off = function(){
+	},
+ 	off = function(){
 	// something else here
-}
+	}
+};
 
