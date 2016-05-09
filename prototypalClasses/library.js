6564325203b4a1a92b56fa137e2cbb684a608602
David Vite <vitebasket10@gmail.com>
2016-05-09 11:57:24 -0600

    Explain the provided container prototype and refactor the code

--- prototypalClasses/library.js
+++ prototypalClasses/library.js
@@ -1,17 +1,21 @@
-// Check Evernote prototypal classes
-// The steps for making the class in this prototypal pattern are pretty clear :
-// - A function that allow us to make instances (line 1-5 above)
-// - A line in that function that generates the new instance object. (line 2 above)
-// - a delegation from the new to some prototype object. ( the parenthesis in line 2 to the prototype object in line -10).
-// - And some logic for augmenting the object with properties that make it unique from all the other objects of the same class.
+// Since this pattern is so common, the language designer decided to add official conventions to support it.
+// We are making a methods object in from line 6 to line 10, right ? and we are adding this object as a property
+ // of the Car function object? If we are planning to use prototypes for all of our classes,
+  // we’ll probably be doing this every time. Since building a holder object for methods and attaching it as a property
+   // to the constructor function is so common, the language does this for you automatically.
+// Whenever a function is created , it will have an object attached to it, that you can use as a container
+ // for methods just in case you plan on using that function, to build instances of a class.
+// The crazy thing is,  this occasionally handy property, the one the language is going to build for you,
+ // is not stored at the key.method. although we had chosen to store our handy method container object at the key.methods .
+  // The default object that comes with every function, is stored at the key.prototype.
 
 var Car = function(loc){
-	var obj= Object.create(Car.methods);
+	var obj= Object.create(Car.prototype);
 	obj.loc = loc;
 	return obj;
 	};
 
-Car.methods = {
+Car.prototype = {
 	move : function(){
 	this.obj++;
 	},
